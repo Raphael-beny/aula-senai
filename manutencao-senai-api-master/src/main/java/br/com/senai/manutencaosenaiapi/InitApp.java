1301c221000f083c249ebebe45cd97f15bf349ef
Raphael Fernandes <raphaelvfernandes@gmail.com>
2022-05-12 19:51:37 -0300

    criando a branch monolito

--- manutencao-senai-api-master/src/main/java/br/com/senai/manutencaosenaiapi/InitApp.java
+++ manutencao-senai-api-master/src/main/java/br/com/senai/manutencaosenaiapi/InitApp.java
@@ -1,18 +1,10 @@
 package br.com.senai.manutencaosenaiapi;
 
-import java.time.LocalDate;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-
-import br.com.senai.manutencaosenaiapi.entity.Tecnico;
-import br.com.senai.manutencaosenaiapi.repository.PecasRepository;
-import br.com.senai.manutencaosenaiapi.repository.TecnicosRepository;
-import br.com.senai.manutencaosenaiapi.service.TecnicoService;
 
 @SpringBootApplication
 public class InitApp {
@@ -21,10 +13,6 @@ public class InitApp {
 		SpringApplication.run(InitApp.class, args);
 		
 	}
-	
-	@Autowired
-	private TecnicoService tecnicoService;
-	
 	@Bean	
 	public CommandLineRunner commandLineRunner(ApplicationContext ac) {
 		return args -> {
